5c78bb4d3dec1580d912b4d7b68e31052b333d83
DaDog <sebnunez95@gmail.com>
2024-07-27 02:41:14 -0400

    Commit inicial
    reciclaje del codigo del otro projecto

---
+++ wipegame/code/Components/Player.cs
@@ -0,0 +1,197 @@
+using Sandbox;
+using Sandbox.Citizen;
+
+public sealed class Player : Component
+{
+	[Property]
+	[Category( "Components" )]
+	public GameObject Camera { get; set; }
+
+
+	[Property]
+	[Category( "Components" )]
+	public CharacterController Controller { get; set; }
+
+
+	[Property]
+	[Category( "Components" )]
+	public CitizenAnimationHelper Animator { get; set; }
+
+
+	/// <summary>
+	/// Velocidad al Caminar (units per second)
+	/// </summary>
+	[Property]
+	[Category( "Stats" )]
+	[Range( 0f, 400f, 1f )]
+	public float WalkSpeed { get; set; } = 120f;
+
+	
+	/// <summary>
+	/// Velocidad al Correr (units per second)
+	/// </summary>
+	[Property]
+	[Category( "Stats" )]
+	[Range( 0f, 800f, 1f )]
+	public float RunSpeed { get; set; } = 250f;
+
+
+	/// <summary>
+	/// Fuerza del salto (units per second)
+	/// </summary>
+	[Property]
+	[Category( "Stats" )]
+	[Range( 0f, 1000f, 10f )]
+	public float JumpStrenght { get; set; } = 400f;
+	/// <summary>
+	/// Dmg por Golpe
+	/// </summary>
+	[Property]
+	[Category( "Stats" )]
+	[Range( 0f, 5f, 0.1f )]
+	public float PunchSTR { get; set; } = 1f;
+	
+	/// <summary>
+	/// Cooldown por golpe
+	/// </summary>
+	[Property]
+	[Category( "Stats" )]
+	public float PunchCooldown { get; set; } = 0.5f;
+
+	/// <summary>
+	/// Rango del golpe
+	/// </summary>
+	[Property]
+	[Category( "Stats" )]
+	public float PunchRange { get; set; } = 50f;
+
+
+
+	/// <summary>
+	/// Where the camera rotates around and the aim originates from
+	/// </summary>
+	[Property]
+	public Vector3 EyePosition { get; set; }
+	public Vector3 EyeWorldPosition => Transform.Local.PointToWorld( EyePosition );
+
+	public Angles EyeAngles { get; set; }
+	Transform _initialCameraTransform;
+	TimeSince _lastPunch;
+
+	protected override void DrawGizmos()
+	{
+		if (!Gizmo.IsSelected) return;
+		 
+		var draw = Gizmo.Draw;
+
+		draw.LineSphere( EyePosition, 10f );
+		draw.LineCylinder( EyePosition, EyePosition + Transform.Rotation.Forward * PunchRange, 5f, 5f, 10 );
+
+	}
+
+	protected override void OnUpdate()
+	{
+		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -80f, 80f ) );
+		Transform.Rotation = Rotation.FromYaw( EyeAngles.yaw );
+		if ( Camera != null )
+		{
+			var cameraTransform = _initialCameraTransform.RotateAround( EyePosition, EyeAngles.WithYaw( 0f ) );
+			var cameraPosition = Transform.Local.PointToWorld( cameraTransform.Position );
+			var cameraTrace = Scene.Trace.Ray( EyeWorldPosition, cameraPosition )
+				.Size( 5f )
+				.IgnoreGameObjectHierarchy( GameObject )
+				.WithoutTags( "player" )
+				.Run();
+			Camera.Transform.Position = cameraTrace.EndPosition;
+			Camera.Transform.LocalRotation = cameraTransform.Rotation;
+		}
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		if ( Controller == null ) return;
+
+		var wishSpeed = Input.Down( "Run" ) ? RunSpeed : WalkSpeed;
+		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * Transform.Rotation;
+
+		Controller.Accelerate( wishVelocity );
+
+		if ( Controller.IsOnGround )
+		{
+			Controller.Acceleration = 10f;
+			Controller.ApplyFriction( 5f, 20f );
+
+			if ( Input.Pressed( "Jump" ) )
+			{
+				Controller.Punch( Vector3.Up * JumpStrenght );
+
+				if ( Animator != null )
+					Animator.TriggerJump();
+			}
+		}
+		else
+		{
+			Controller.Acceleration = 5f;
+			Controller.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta;
+		}
+
+		Controller.Move();
+
+		if ( Animator != null )
+		{
+			Animator.IsGrounded = Controller.IsOnGround;
+			Animator.WithVelocity( Controller.Velocity );
+
+			if ( _lastPunch >= 2f )
+				Animator.HoldType = CitizenAnimationHelper.HoldTypes.None;
+		}
+		if ( Input.Pressed( "Punch" ) && _lastPunch >= PunchCooldown )
+			Punch(); 
+	}
+	protected override void OnStart()
+	{
+		if ( Camera != null )
+			_initialCameraTransform = Camera.Transform.Local;
+		if ( Components.TryGet<SkinnedModelRenderer>( out var model ) )
+		{
+			var clothing = ClothingContainer.CreateFromLocalUser();
+			clothing.Apply( model );
+		}
+	}
+	public void Punch()
+	{
+		if ( Animator != null )
+		{ 
+		Animator.HoldType = CitizenAnimationHelper.HoldTypes.Punch;
+		Animator.Target.Set( "b_attack", true );
+		}
+		var punchTrace = Scene.Trace
+			.FromTo( EyeWorldPosition, EyeWorldPosition + EyeAngles.Forward * PunchRange )
+			.Size( 10f )
+			.WithoutTags( "player" )
+			.IgnoreGameObjectHierarchy( GameObject )
+			.Run();
+		if ( punchTrace.Hit )
+			if ( punchTrace.GameObject.Components.TryGet<UnitInfo>( out var unitInfo ) )
+				unitInfo.Damage( PunchSTR );
+		
+
+		_lastPunch = 0f;
+	}
+	protected override void OnEnabled()
+	{
+		base.OnEnabled();
+	}
+	protected override void OnDisabled()
+	{
+		base.OnDisabled();
+	}
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+	}
+
+}
---
+++ wipegame/code/Components/UnitInfo.cs
@@ -0,0 +1,91 @@
+using Sandbox;
+
+public enum UnitType
+{
+	/// <summary>
+	/// Enviromental units or resources
+	/// </summary>
+	[Icon( "check_box_outline_blank" )]
+	None,
+	/// <summary>
+	/// Player and turrets
+	/// </summary>
+	[Icon( "boy" )]
+	Player,
+	/// <summary>
+	/// Enemies
+	/// </summary>
+	[Icon( "cloud_queue" )]
+	Snot
+}
+[Icon( "psychology" )]
+
+public sealed class UnitInfo : Component
+{
+	[Property]
+	public UnitType Team { get; set; }
+	[Property]
+	[Range( 0.1f, 10f, 0.1f )]
+	public float MaxHealth { get; set; } = 5f;
+	/// <summary>
+	/// HP Regen Fuera de combat
+	/// </summary>
+	[Property]
+	[Range( 0f, 2f, 0.1f )]
+	public float HealthRegenAmount { get; set; } = 0.5f;
+
+	/// <summary>
+	/// Segundos antes de HP Regen
+	/// </summary>
+	[Property]
+	[Range( 0f, 2f, 1f )]
+	public float HealthRegenTimer { get; private set; }
+
+	public float Health { get; private set; }
+
+	public bool Alive { get; private set; } = true;
+
+	TimeSince _lastDamage;
+	TimeUntil _nextHeal;
+
+
+	protected override void OnUpdate()
+	{
+		if ( _lastDamage >= HealthRegenTimer && Health != MaxHealth && Alive )
+		{
+			if ( _nextHeal )
+			{
+				Damage( -HealthRegenAmount );
+				_nextHeal = 1f;
+			}
+		}
+	}
+	protected override void OnStart()
+	{
+		Health = MaxHealth;
+	}
+
+	/// <summary>
+	/// Damage the Unit, Clamped, Heal if set to negative
+	/// </summary>
+	/// <param name="damage"></param>
+	public void Damage( float damage )
+	{
+		if ( !Alive ) return;
+
+		Health = MathX.Clamp( Health - damage, 0f, MaxHealth );
+
+		if ( damage > 0 )
+			_lastDamage = 0f;
+
+		if ( Health <= 0 )
+			Krill();
+	}
+	public void Krill()
+	{
+		Health = 0f;
+		Alive = false;
+		GameObject.Destroy();
+	}
+
+}
